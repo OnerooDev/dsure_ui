2ef0295673cd93d068caa317f575d79b80376c57
SeerDay <70478004+SeerDay@users.noreply.github.com>
2022-02-10 20:32:56 +0700

    production v. 0.1

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
-export const Token_address = '0x93c96c8d8f41318dc2b7872d78676c16e69cbbcf';
-export const Dsure_address = '0xd1ad7b176887044089F116a38775D2761f82918B';
+export const Token_address = '0x55d398326f99059ff775485246999027b3197955';
+export const Dsure_address = '0x2204c224fd584C6149fe6F2aE63371C7dFC020b5';
 export const price_1 = '9750000000000000000000';
 export const price_2 = '13950000000000000000000';
-export const node_url =  'https://eth-kovan.alchemyapi.io/v2/9WPyPZ-Atu-Viio5c934dF8ISTvdUr';
-export const node_network_id = 42;
+export const node_url =  'https://bsc-dataseed3.binance.org/';
+export const node_network_id = 56;
 export const URL = 'https://app.dsure.io';
